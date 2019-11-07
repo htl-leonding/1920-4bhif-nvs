0ec5df35fab3627679e86686245c2e60dbe39437
htl-leonding <t.stuetz@htl-leonding.ac.at>
2019-11-07 14:20:56 +0100

    Projects Vehicle and Restprimer added

---
+++ livecoding/Vehicle/src/main/java/at/htl/vehicle/model/Vehicle.java
@@ -0,0 +1,36 @@
+package at.htl.vehicle.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Vehicle {
+
+    private String brand;
+    private String type;
+
+    public Vehicle() {
+    }
+
+    //region getter and setter
+    public Vehicle(String brand, String type) {
+        this.brand = brand;
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    //endregion
+}
