403dd574190afb401e9f69d0472972f3ee3ca6c6
htl-leonding <t.stuetz@htl-leonding.ac.at>
2019-11-14 22:23:04 +0100

    Unterricht am 14-11-2019; POST-Request

--- livecoding/PersonRest/src/main/java/at/htl/person/rest/PersonEndpoint.java
+++ livecoding/PersonRest/src/main/java/at/htl/person/rest/PersonEndpoint.java
@@ -2,14 +2,13 @@ package at.htl.person.rest;
 
 import at.htl.person.model.Person;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.net.URI;
 import java.util.List;
 
 @Path("person")
@@ -26,18 +25,32 @@ public class PersonEndpoint {
                 .getResultList();
     }
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Person findByName(@QueryParam("name") String name) {
-        return em
-                .createNamedQuery("Person.findByName",Person.class)
-                .setParameter("NAME", name)
-                .getSingleResult();
-    }
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Person findByName(@QueryParam("name") String name) {
+//        return em
+//                .createNamedQuery("Person.findByName",Person.class)
+//                .setParameter("NAME", name)
+//                .getSingleResult();
+//    }
 
     @GET
     @Path("demo")
     public Response returnResponse() {
         return Response.accepted().build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response createPerson(final @Context UriInfo uriInfo, Person person) {
+        Person p = em.merge(person);
+        URI uri = uriInfo.getAbsolutePathBuilder()
+                .path("/" + p.getId())
+                .build();
+        return Response
+                .created(uri)
+                .header("operation", "object created")
+                .build();
     }
 }
