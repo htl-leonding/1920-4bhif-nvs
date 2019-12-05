dcb05cd831e472cde9b2a646d99b9e0b551709bb
htl-leonding <t.stuetz@htl-leonding.ac.at>
2019-12-05 16:27:42 +0100

    csv persisiting in db completed

--- livecoding/PersonRest/src/main/java/at/htl/person/business/InitBean.java
+++ livecoding/PersonRest/src/main/java/at/htl/person/business/InitBean.java
@@ -10,11 +10,19 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Transient;
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.stream.Stream;
 
 @ApplicationScoped
 public class InitBean {
+
+    private static final String FILE_NAME = "got.csv";
 
     public InitBean() {
     }
@@ -30,6 +38,17 @@ public class InitBean {
         em.persist(franz);
         em.persist(new Person("Susi", LocalDate.of(2001, Month.SEPTEMBER, 3)));
         em.persist(new Person("Mimi", LocalDate.of(2001, Month.MAY, 31)));
+
+        URL url = Thread.currentThread().getContextClassLoader()
+                .getResource(FILE_NAME);
+        try (Stream<String> stream = Files
+                .lines(Paths
+                        .get(url.getPath()), StandardCharsets.UTF_8)) {
+            stream.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
--- livecoding/ReadCsv/src/main/java/at/htl/readcsv/business/InitBean.java
+++ livecoding/ReadCsv/src/main/java/at/htl/readcsv/business/InitBean.java
@@ -1,16 +1,50 @@
 package at.htl.readcsv.business;
 
+import at.htl.readcsv.model.Person;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Initialized;
 import javax.enterprise.event.Observes;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @ApplicationScoped
+@Transactional
 public class InitBean {
+
+    @PersistenceContext
+    EntityManager em;
+
+    private static final String FILE_NAME = "got.csv";
 
     private void init(
             @Observes
             @Initialized(ApplicationScoped.class) Object object) {
-        System.out.println("Hello");
+        readCsv(FILE_NAME);
+    }
+
+
+    private void readCsv(String fileName) {
+        URL url = Thread.currentThread().getContextClassLoader()
+                .getResource(fileName);
+        try (Stream<String> stream = Files.lines(Paths.get(url.getPath())
+                , StandardCharsets.UTF_8)) {
+            stream
+                    .skip(1)
+                    .map(line -> line.split(";"))
+                    .map(elem -> new Person(elem[0], elem[1], elem[2]))
+                    //.forEach(System.out::println);
+                    .forEach(em::merge);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
