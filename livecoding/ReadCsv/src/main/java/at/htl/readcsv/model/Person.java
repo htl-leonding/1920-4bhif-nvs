316867a132c55471849961ebb2d9d56f098b45b8
htl-leonding <t.stuetz@htl-leonding.ac.at>
2019-12-05 16:28:16 +0100

    csv persisiting in db completed

---
+++ livecoding/ReadCsv/src/main/java/at/htl/readcsv/model/Person.java
@@ -0,0 +1,66 @@
+package at.htl.readcsv.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String city;
+    private String house;
+
+    //region constructors
+    public Person(String name, String city, String house) {
+        this.name = name;
+        this.city = city;
+        this.house = house;
+    }
+
+    public Person() {
+    }
+    //endregion
+
+    //region GEtter and Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+    //endregion
+
+    @Override
+    public String toString() {
+        return String.format("%s, %s of %s", name, house, city);
+    }
+}
